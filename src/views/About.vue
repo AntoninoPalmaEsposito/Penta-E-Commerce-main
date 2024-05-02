15dde07d7e8f13409e318e58fd3ff2ee7051d15d
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-05-02 19:07:29 +0200

    h

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,33 +3,26 @@
     <Section>
     
         <!-- TITOLO -->
-        <div class="m-[60px]">
-            <h1 class="text-grande">Titolo</h1>
+        <div class="mt-[50px] md:mt-[80px] px-[30px] md:px-[100px]">
+            <h1 class="text-[40px] md:text-[45px] font-extrabold text-[rgba(0,0,0,0.66)]">Chi siamo?</h1>
         </div>
 
         <!-- ABOUT DESCRIPTION -->
-        <div class="m-[60px] text-piccolo ">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, voluptates nemo omnis a tenetur rem ad velit? Asperiores molestiae debitis autem deserunt nostrum nihil aliquam atque provident dignissimos rerum! Deserunt!
-            Nihil qui facere error ad delectus eos rem sapiente illum voluptatum pariatur sint corrupti ratione officiis id repellat et eum facilis voluptate quia fuga incidunt</p>
-            
-           <p class="pt-[20px]">Unde eum accusantium esse hic est velit fuga delectus impedit laborum, aspernatur officia cum, repellendus, iure debitis facilis. Cupiditate doloribus maxime quos explicabo! Possimus quia consequatur obcaecati expedita! Illum, quos!
-            Inventore, architecto consequatur accusantium voluptatum nam beatae id libero quae! Optio aliquam harum culpa numquam quis libero excepturi nam est magnam qui non impedit et minima, nihil provident? Magni, non!
-            Fuga animi dolorem voluptatem doloribus ea, ipsum rem nobis libero ab illum quibusdam iste consequatur rerum. Sapiente debitis eum, delectus beatae molestias obcaecati animi illum! Quia expedita tempora quis porro?</p>
+        <div class="mt-[10px] md:mt-[20px] text-[18px] md:text-[20px] px-[30px] md:px-[100px] ">
+            <p>Siamo un team di programmatori giovani e appassionati che ha creato questo sito con l’obiettivo di offrire ai nostri clienti un’esperienza di shopping online fluida e intuitiva.  
+                Il nostro team è composto da programmatori talentuosi e creativi. Abbiamo una profonda conoscenza delle tecnologie di sviluppo web, dai linguaggi di programmazione alle basi di dati. Siamo sempre alla ricerca di nuove sfide e soluzioni innovative per migliorare il nostro e-commerce. <br> <br> 
+                Abbiamo progettato il nostro sito con un’attenzione particolare all’usabilità e all’estetica. L’interfaccia utente è pulita, intuitiva e responsive, garantendo una piacevole esperienza di navigazione per i nostri visitatori.  
+                Essendo un team giovane, siamo agili e pronti a rispondere rapidamente alle esigenze dei nostri clienti. Possiamo adattarci facilmente ai cambiamenti del mercato e alle richieste degli utenti. <br> <br>  
+                La sicurezza dei dati dei nostri clienti è fondamentale. Abbiamo implementato misure di sicurezza avanzate per proteggere le informazioni personali e le transazioni online.  
+                Collaboriamo con fornitori affidabili per offrire una vasta gamma di prodotti di alta qualità. Dalle ultime tendenze di moda agli articoli per la casa, troverete tutto ciò di cui avete bisogno nel nostro negozio.
+            </p>
 
-            <p class="pt-[20px]">Unde eum accusantium esse hic est velit fuga delectus impedit laborum, aspernatur officia cum, repellendus, iure debitis facilis. Cupiditate doloribus maxime quos explicabo! Possimus quia consequatur obcaecati expedita! Illum, quos!
-            Inventore, architecto consequatur accusantium voluptatum nam beatae id libero quae! Optio aliquam harum culpa numquam quis libero excepturi nam est magnam qui non impedit et minima, nihil provident? Magni, non!
-            Fuga animi dolorem voluptatem doloribus ea, ipsum rem nobis libero ab illum quibusdam iste consequatur rerum. Sapiente debitis eum, delectus beatae molestias obcaecati animi illum! Quia expedita tempora quis porro?</p>
+            <p>
+                Grazie per aver scelto di fare shopping con noi! Speriamo che il nostro impegno per l’eccellenza vi conquisti e che diventiate parte della nostra crescente famiglia di clienti soddisfatti. 
+            </p>
         </div>
 
-        <!-- CONTATTI -->
-        <div class="m-[60px] pb-[40px] pt-[60px]">
-
-            <h2 class="text-grande">Contatti</h2>
-            
-            <p class="pt-[20px] text-piccolo">Via xxxxxxxxxx</p>
-
-            <p class="pt-[20px] text-piccolo">n-telefono:123456789</p>
-        </div>
+       
 
         
     </Section>
