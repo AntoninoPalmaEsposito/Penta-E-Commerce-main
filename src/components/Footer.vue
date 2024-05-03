0051c2f173cff5e2899cdc4783de49e0df493613
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-05-03 12:24:46 +0200

    d

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,57 +1,131 @@
 <template>
 
     <footer class="text-black bg-grigio mt-[100px]">
-      <div class="mx-auto w-full max-w-screen-xl pt-[20px]">
-        
+        <div class="mx-auto md:mx-0 w-full max-w-screen-xl pt-[20px]">
 
 
-          <div class="grid grid-cols-2 gap-8  px-10 py-6 lg:py-8 min-[641px]:grid-cols-4 text-[rgba(0,0,0,0.77)] ">
+            <!-- footer mob -->
+            <div
+                class="grid md:hidden grid-cols-2 gap-8  px-10 py-6 lg:py-8 min-[641px]:grid-cols-4 text-[rgba(0,0,0,0.77)] ">
 
-          
+                <div class="max-[640px]:text-center">
+                    <h2 class="mb-4 text-base font-bold  uppercase ">Legal</h2>
+                    <ul class=" font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Privacy Policy</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Licensing</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Shipping</a>
+                        </li>
 
-        
-          <div class="max-[640px]:text-center">
-              <h2 class="mb-4 text-base font-bold  uppercase ">Legal</h2>
-              <ul class=" font-medium">
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Privacy Policy</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Licensing</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Shipping</a>
-                  </li>
-                  
-              </ul>
-          </div>
+                    </ul>
+                </div>
 
-          <div class="max-[640px]:text-center">
-              <h2 class="mb-4  text-base font-bold  uppercase ">Social</h2>
-              <ul class=" font-medium">
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Discord Server</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Twitter</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Facebook</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Instagram</a>
-                  </li>
-              </ul>
-          </div>
+                <div class="max-[640px]:text-center">
+                    <h2 class="mb-4  text-base font-bold  uppercase ">Social</h2>
+                    <ul class=" font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Discord Server</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Twitter</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Facebook</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Instagram</a>
+                        </li>
+                    </ul>
+                </div>
 
-          
+
+            </div>
+
+
+
+
+
+
+
+            <!-- footer dek -->
+            <div
+                class="hidden md:flex flex-col  w-screen text-[rgba(0,0,0,0.77)] pt-[30px] ">
+
+                <div class="flex justify-between items-center">
+                    <RouterLink to="/" class="text-grande pl-[100px] pr-[100px]"><svg class="w-[60px]" viewBox="0 0 500 500"
+                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_288_4)">
+                                <path
+                                    d="M320.6 383.3H159.5C153.2 383.3 147.3 386.8 144.3 392.3L106 461.6C103 467.1 105 474 110.5 477C112.2 477.9 114.1 478.4 116 478.4H321.7C346.9 478.4 367.7 458.8 369.2 433.6C370.7 406.1 348.4 383.3 320.6 383.3Z"
+                                    fill="#294F94" />
+                                <path
+                                    d="M273 28.6999C250.6 14.8999 221.1 21.5999 207.3 43.9999C207.3 44.1999 207.1 44.4 206.9 44.4L7.2001 405.7C-6.6999 428.1 0.200104 457.5 22.5001 471.4C30.0001 476.1 38.8001 478.5 47.6001 478.4C64.2001 478.5 79.6001 469.9 88.1001 455.6L287.9 94.2999C301.9 72.0999 295.2 42.6999 273 28.6999Z"
+                                    fill="#294F94" />
+                                <path
+                                    d="M493.1 406.1L408.6 253.6C394.6 231.5 365.2 224.5 343.1 238.5C321 252.1 314 281 327.2 303.3L411.7 455.8C420.2 469.9 435.5 478.5 452 478.5C474.1 478.6 493.4 463.4 498.7 441.9C501.6 429.3 499.7 416.7 493.1 406.1ZM315.8 137C289.7 137 268.6 158.1 268.6 184.2C268.6 210.3 289.7 231.4 315.8 231.4C341.9 231.4 363 210.3 363 184.2C363.4 158.2 342.2 137 315.8 137Z"
+                                    fill="#294F94" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_288_4">
+                                    <rect width="500" height="500" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </RouterLink>
+
+                    <div class="flex pr-[35vw]">
+                        <h2 class="mb-4 text-[20px] mr-[50px] font-bold  uppercase ">Legal</h2>
+                        <ul class=" text-[18px] flex">
+                            <li class="mb-4">
+                                <a href="#" class="underline underline-offset-4 mr-[40px] text-nowrap">Privacy Policy</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="#" class=" underline underline-offset-4 mr-[40px] text-nowrap">Licensing</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="#" class=" underline underline-offset-4 mr-[40px] text-nowrap">Terms &amp; Conditions</a>
+                            </li>
+                            <li class="mb-4">
+                                <a href="#" class=" underline underline-offset-4 text-nowrap">Shipping</a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+
+
+
+                <div class="flex justify-end pr-[70px] pt-[10px] pb-[10px]">
+                    <ul class=" flex">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline mr-[20px]">Discord Server</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline mr-[20px]">Twitter</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline mr-[20px]">Facebook</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Instagram</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
         </div>
-      </div>
-</footer>
+    </footer>
 
 
- 
+
 </template>
